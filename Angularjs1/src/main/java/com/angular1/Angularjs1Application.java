dabe1af90f517b433d0d945d16398b5b3ba4ceb5
administrator-pc <administrator-pc@CILNSG-JAVA5>
2017-08-05 10:12:03 +0530

    First Commit

---
+++ Angularjs1/src/main/java/com/angular1/Angularjs1Application.java
@@ -0,0 +1,12 @@
+package com.angular1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Angularjs1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Angularjs1Application.class, args);
+	}
+}
